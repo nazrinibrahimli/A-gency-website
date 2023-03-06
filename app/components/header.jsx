342b8577def84fafd06916a447a3d9330e0c70f6
Nazrin İbrahimli <nibrahimli4676@ada.edu.az>
2023-03-06 16:25:00 +0400

    changes to footer and responsiveness

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -5,10 +5,10 @@ import Button from '@mui/material/Button';
 const Header = () => {
   return (
     <div>
-      <header className='flex p-[1rem] bg-gray-light '>
-      <h2 className='font-serif text-bold px-[3rem] z-20'> <span className='text-white'>A'</span>gency</h2>
-      <div className='w-[12rem] h-[12rem] bg-blue rounded-full absolute left-[-6rem] top-[-6rem] z-10'></div> 
-      <nav className='m-auto '>
+      <header className='flex p-[1rem] bg-gray-light overflow-hidden relative'>
+      <h2 className='font-serif text-bold px-[2.2rem] z-20'> <span className='text-white'>A'</span>gency</h2>
+      <div className='w-[13rem] h-[13rem] bg-blue rounded-full absolute left-[-6rem] top-[-9rem] z-10'></div> 
+      <nav className='ml-auto '>
       <Link href="/" underline="none" className='text-xl text-blue  px-3 font-sans'>
           {'Home'}
         </Link>
@@ -24,14 +24,16 @@ const Header = () => {
         <Link href="#" underline="none" className='text-2xl text-blue lowercase px-3 font-serif'>
           {'Insight'}
         </Link> */}
-        <Link href="#" underline="none" className='text-xl text-gray-dark  px-3 font-sans'>
+        {/* <Link href="#" underline="none" className='text-xl text-gray-dark  px-3 font-sans'>
           {'Contact Us'}
-        </Link>        </nav>
-        <Button variant="contained" className='mr-[3rem] flex font-sans bg-blue rounded-md h-[3rem] hover:bg-gray-dark shadow-none'>
+
+        </Link>       */}
+          </nav>
+        {/* <Button variant="contained" className='mr-[3rem] flex font-sans bg-blue rounded-md h-[3rem] hover:bg-gray-dark shadow-none'>
         <Link href="#" underline="none" className='text-[white] font-sans'>
           {'Sign in'}
         </Link>
-        </Button>
+        </Button> */}
 
       </header>
       
