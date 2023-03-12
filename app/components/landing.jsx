bd22ce68d2fe0af184cbc5f21b00b53d8a26f098
Nazrin İbrahimli <nibrahimli4676@ada.edu.az>
2023-03-12 13:16:39 +0400

    chnages to buttons

--- app/components/landing.jsx
+++ app/components/landing.jsx
@@ -13,11 +13,11 @@ const LandingPage = () => {
      
       <p className=' w-[26rem] my-[3rem]'>We specialize in destkop and mobile applications, CEO, marketing, AI solutions, cybersecutity.</p>
       <div className="lg:flex sm:none">
-     <div className='w-[10.5rem] font-sans bg-[#252849]  rounded-md mr-[1rem] h-[3rem] hover:bg-gray-dark'>
-       <Button variant="contained" className='h-full w-full'><AppleIcon sx={{mr:1}}/>  App store</Button>
+     <div className='w-[10.5rem] font-sans bg-[#252849]  text-[white] rounded-md mr-[1rem] h-[3rem] '>
+       <Button variant="" className='h-full w-full'><AppleIcon sx={{mr:1}}/>  App store</Button>
       </div>
       <div className='w-[10.5rem] sm:mt-[0.8rem] lg:mt-0 font-sans rounded-md h-[3rem] border-[#252849] text-[#252849]'>
-      <Button variant="outlined" className='h-full w-full'><AndroidIcon sx={{mr:1}}/> Play store</Button>
+      <Button variant="outlined" className='h-full w-full border-[#252849] text-[#252849] '><AndroidIcon sx={{mr:1}}/> Play store</Button>
       </div>
      </div>
 
