d38e82c0bcd5f5eb5b8b23b35abcfd278b08f012
Nazrin İbrahimli <nibrahimli4676@ada.edu.az>
2023-04-14 11:21:08 +0400

    changes to layout/button

--- app/components/cto.jsx
+++ app/components/cto.jsx
@@ -10,9 +10,12 @@ const cto = (props) => {
               <Grid item xs={12} md={12} lg={12} sx={{textAlign:'center',}}  >
                     <img src={props.icon}/>
                   <h1 className='text-[white] '>{props.title}</h1>
+                  <div className='sm:mt-[0.8rem] lg:mt-0 font-sans rounded-md h-[3rem] border-[#252849] border-[1px] text-[#252849]'>
+
                     <Button size="large" variant='outlined' className='mt-[2rem] font-sans rounded-sm border-5	border-[white]'>
                     <p className='text-[white] text-[0.8rem]'>{props.cto}</p>                   
                      </Button>
+                    </div> 
               </Grid>
     </div>
   )
