a8722209c6d705f86decefeace0b34565d931a14
Nazrin İbrahimli <nibrahimli4676@ada.edu.az>
2023-04-26 23:34:06 +0400

    changes to layout in home page

--- app/components/landing.jsx
+++ app/components/landing.jsx
@@ -9,7 +9,7 @@ import AndroidIcon from '@mui/icons-material/Android';
 import bgImage from "../../public/static/app.jpg"
 const LandingPage = () => {
   return (
-    <div className='lg:px-[9.5rem] px-[3rem] pt-[3rem] bg-fixed bg-center bg-no-repeat bg-[url("/static/bg.jpg")]  bg-cover  h-[40rem] relative overflow-hidden '>
+    <div className='lg:px-[9.5rem] px-[3rem] pt-[6rem] bg-fixed bg-center bg-no-repeat bg-[url("/static/bg.jpg")]  bg-cover  h-[40rem] relative overflow-hidden '>
       <h1 className='text-center text-6xl  text-[white] font-bold'>Improve the way you charge, with only <span className=' text-[white]'>one click.</span></h1>
      
       <p className='px-[4rem] text-center  text-[white] my-[3rem]'>We specialize in destkop and mobile applications, CEO, marketing, AI solutions, cybersecutity.</p>
